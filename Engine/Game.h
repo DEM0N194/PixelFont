213192dccbe2d37548e3b1d357a4d6389bfc0600
DEM0N194 <KevinLacko.KL@gmail.com>
2017-04-27 08:39:00 +0200

    Moved the PixelFont Functions to Graphics.h and added Color.h parameter

--- Engine/Game.h
+++ Engine/Game.h
@@ -47,49 +47,5 @@ private:
 	/********************************/
 	/*         Pixel Font           */
 	/********************************/
-	void Block(int x, int y, int r, int g, int b);
-
-	void chA(int x, int y, int r, int g, int b);
-	void chB(int x, int y, int r, int g, int b);
-	void chC(int x, int y, int r, int g, int b);
-	void chD(int x, int y, int r, int g, int b);
-	void chE(int x, int y, int r, int g, int b);
-	void chF(int x, int y, int r, int g, int b);
-	void chG(int x, int y, int r, int g, int b);
-	void chH(int x, int y, int r, int g, int b);
-	void chI(int x, int y, int r, int g, int b);
-	void chJ(int x, int y, int r, int g, int b);
-	void chK(int x, int y, int r, int g, int b);
-	void chL(int x, int y, int r, int g, int b);
-	void chM(int x, int y, int r, int g, int b);
-	void chN(int x, int y, int r, int g, int b);
-	void chO(int x, int y, int r, int g, int b);
-	void chP(int x, int y, int r, int g, int b);
-	void chQ(int x, int y, int r, int g, int b);
-	void chR(int x, int y, int r, int g, int b);
-	void chS(int x, int y, int r, int g, int b);
-	void chT(int x, int y, int r, int g, int b);
-	void chU(int x, int y, int r, int g, int b);
-	void chV(int x, int y, int r, int g, int b);
-	void chW(int x, int y, int r, int g, int b);
-	void chX(int x, int y, int r, int g, int b);
-	void chY(int x, int y, int r, int g, int b);
-	void chZ(int x, int y, int r, int g, int b);
-
-	void ch0(int x, int y, int r, int g, int b);
-	void ch1(int x, int y, int r, int g, int b);
-	void ch2(int x, int y, int r, int g, int b);
-	void ch3(int x, int y, int r, int g, int b);
-	void ch4(int x, int y, int r, int g, int b);
-	void ch5(int x, int y, int r, int g, int b);
-	void ch6(int x, int y, int r, int g, int b);
-	void ch7(int x, int y, int r, int g, int b);
-	void ch8(int x, int y, int r, int g, int b);
-	void ch9(int x, int y, int r, int g, int b);
-
-	void chPoint(int x, int y, int r, int g, int b);
-	void chApostrophe(int x, int y, int r, int g, int b);
-	void chExMark(int x, int y, int r, int g, int b);
-	void chQMark(int x, int y, int r, int g, int b);
-	void chDash(int x, int y, int r, int g, int b);
+	
 };
